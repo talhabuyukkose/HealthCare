ae6720ec6248dcc346492e9ab13bcc330037982e
Talha Buyukkose <talha.buyukkose@sadeyazilim.com>
2024-03-22 11:36:32 +0300

    asd

--- src/Libraries/HealthCare.Core.Domain/Entities/Doctor.cs
+++ src/Libraries/HealthCare.Core.Domain/Entities/Doctor.cs
@@ -5,6 +5,7 @@ namespace HealthCare.Core.Domain.Entities
     public class Doctor : BaseEntity
     {
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
         public string Title { get; set; }
         public int MedicalUnitID { get; set; }
